74347e1433dfc57c8f72e9aadc6333632313a3f3
Ajay <aamodi@asu.edu>
2016-01-22 14:30:50 -0700

    randomizing mood

--- src/main/java/edu/asu/diging/tutorial/spring/service/MoodService.java
+++ src/main/java/edu/asu/diging/tutorial/spring/service/MoodService.java
@@ -1,13 +1,34 @@
 package edu.asu.diging.tutorial.spring.service;
  
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Service;
- 
+
 import edu.asu.diging.tutorial.spring.domain.Mood;
  
 @Service
 public class MoodService {
  
-    public Mood getCurrentMood() {
-        return new Mood("happy");
+    private List<String> moodList = new ArrayList<String>();
+    
+    @PostConstruct
+    public void init() {
+        moodList.add("Accpted");
+        moodList.add("Angry");
+        moodList.add("Calm");
+        moodList.add("Defensive");
+        moodList.add("Happy");
+        moodList.add("Sad");
     }
+         
+    public Mood getCurrentMood() {
+        Random r = new Random();
+        return new Mood(moodList.get(r.nextInt(moodList.size())));
+    }
+    
+    
 }
